605b6d159c4ae6b0fb2c772146ff0cf460c1aac4
Mateusz Stalmach <mat1986@gmail.com>
2017-02-18 13:59:54 +0100

    Przygotowanie plików

--- api/src/connection.php
+++ api/src/connection.php
@@ -1,2 +1,10 @@
 <?php
 
+require_once 'config.php';
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if ($conn->connect_errno) {
+    die('Can not connect to database:'. $conn->connect_error);
+}
+$conn->query("SET NAMES 'utf8'");
+echo 'udało się';
