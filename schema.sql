50e722409c8c07eb521e2fb879fedf20bae96874
Mateusz Stalmach <mat1986@gmail.com>
2017-02-18 15:00:18 +0100

    add sql schema

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE BooksRent CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE TABLE `books` (
   `id` INT AUTO_INCREMENT,
-  `name` VARCHAR(80) NOT NULL,
+  `title` VARCHAR(80) NOT NULL,
   `author` VARCHAR(30) NOT NULL,
   `description` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`)
